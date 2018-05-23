ff86ef254b893634e94953af5b6bda7138e89ac5
Jean Fiedler <JFI@kd.local>
2018-05-23 15:37:50 +0200

    add _locales to zip fle

--- pack to zip file.cmd
+++ pack to zip file.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
 DEL release.zip
-tools\7za.exe a -tzip release.zip background.js manifest.json img\*.png -mx9
+tools\7za.exe a -tzip release.zip background.js manifest.json img\*.png _locales\* -mx9
